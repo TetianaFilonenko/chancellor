7e6917b67f98ef56816cde2c4891ed30930578ea
Joseph Bridgwater-Rowe <joe@westernmilling.com>
2015-01-24 17:56:07 -0800

    Add dotenv gem. Replace Figaro with dotenv for local ENV vars.

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
-if ENV['smtp_server']
+if ENV['SMTP_SERVER']
   ActionMailer::Base.smtp_settings = {
-    :port => ENV['smtp_port'],
-    :address => ENV['smtp_server'],
-    :user_name => ENV['smtp_login'],
-    :password => ENV['smtp_password'],
+    :port => ENV.fetch('SMTP_PORT'),
+    :address => ENV.fetch('SMTP_SERVER'),
+    :user_name => ENV.fetch('SMTP_LOGIN'),
+    :password => ENV.fetch('SMTP_PASSWORD'),
     :authentication => :plain
   }
   ActionMailer::Base.delivery_method = :smtp
@@ -11,4 +11,4 @@ end
 
 ActionMailer::Base.smtp_settings ||= {}
 ActionMailer::Base.smtp_settings.merge!(ENV.to_h.symbolize_keys.slice(:domain))
-ActionMailer::Base.default_url_options[:host] = ENV['domain']
+ActionMailer::Base.default_url_options[:host] = ENV.fetch('DOMAIN')
