14423b76e759bd785f0388ed60cb1a9a16ea52a6
Joseph Bridgwater-Rowe <joe@westernmilling.com>
2015-01-29 10:18:43 -0800

    Segment. Add Segment for analytics.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,10 @@
 class HomeController < ApplicationController
   def index
     authorize :home_controller, :index?
+
+    Analytics.track(
+      :user_id => current_user.email,
+      :event => 'View Home Page',
+      :properties => {})
   end
 end
